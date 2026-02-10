09979ed76b09e9b49be98a0528a77fa1c47f4a82
Magnus Allison <magnus_alli@hotmail.co.uk>
2026-02-10 09:51:55 -0600

    add vercel analytics

---
+++ wordlist/es/ingredients.ts
@@ -0,0 +1,97 @@
+export const ingredients = [
+	{
+		image: '',
+		english: 'Ingredients',
+		spanish: 'Los ingredientes'
+	},
+	{
+		image: '',
+		english: 'Flour',
+		spanish: 'La harina'
+	},
+	{
+		image: '',
+		english: 'Sugar',
+		spanish: 'El azúcar'
+	},
+	{
+		image: '',
+		english: 'Salt',
+		spanish: 'La sal'
+	},
+	{
+		image: '',
+		english: 'Eggs',
+		spanish: 'Los huevos'
+	},
+	{
+		image: '',
+		english: 'Milk',
+		spanish: 'La leche'
+	},
+	{
+		image: '',
+		english: 'Butter',
+		spanish: 'La mantequilla'
+	},
+	{
+		image: '',
+		english: 'Oil',
+		spanish: 'El aceite'
+	},
+	{
+		image: '',
+		english: 'Water',
+		spanish: 'El agua'
+	},
+	{
+		image: '',
+		english: 'Yeast',
+		spanish: 'La levadura'
+	},
+	{
+		image: '',
+		english: 'Baking powder',
+		spanish: 'El polvo para hornear'
+	},
+	{
+		image: '',
+		english: 'Vanilla',
+		spanish: 'La vainilla'
+	},
+	{
+		image: '',
+		english: 'Cinnamon',
+		spanish: 'La canela'
+	},
+	{
+		image: '',
+		english: 'Chocolate',
+		spanish: 'El chocolate'
+	},
+	{
+		image: '',
+		english: 'Honey',
+		spanish: 'La miel'
+	},
+	{
+		image: '',
+		english: 'Rice',
+		spanish: 'El arroz'
+	},
+	{
+		image: '',
+		english: 'Beans',
+		spanish: 'Los frijoles'
+	},
+	{
+		image: '',
+		english: 'Corn flour',
+		spanish: 'La harina de maíz'
+	},
+	{
+		image: '',
+		english: 'Corn starch',
+		spanish: 'La fécula de maíz'
+	}
+];
